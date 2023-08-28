e5e8767f3f55268c7957fc722eb3b3e568ff6843
plylrnsdy <plylrnsdy@163.com>
2023-08-29 01:52:38 +0800

    fix: can not update cache after create/delete file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
-import * as fs from 'fs'
 import * as path from 'path'
-import * as glob from 'glob'
 import { Notice, Plugin, PluginSettings, decorate } from '@typora-community-plugin/core'
 import { editor } from 'typora'
 import { i18n } from './i18n'
@@ -38,6 +36,7 @@ export default class WikilinkPlugin extends Plugin<WikilinkSettings> {
 
     this.settings.setDefault(DEFAULT_SETTINGS)
 
+
     this.addChild(new WikilinkRenderer(this))
     this.addChild(new WikilinkStyleToggler(this))
     this.addChild(new UseSuggest(this.app, this))
@@ -45,16 +44,36 @@ export default class WikilinkPlugin extends Plugin<WikilinkSettings> {
 
     this.registerSettingTab(new WikilinkSettingTab(this))
 
-    // feat: support open wikilink
+
+    this.register(
+      decorate.afterCall(editor.quickOpenPanel, 'initFileCache', ([paths]) => {
+        const prefixLen = this.app.vault.path.length
+        this.cache.bulkAdd(paths.map(p => p.slice(prefixLen + 1)))
+      }))
+
+    this.register(
+      decorate.afterCall(editor.quickOpenPanel, 'addInitFiles', ([paths]) => {
+        const prefixLen = this.app.vault.path.length
+        this.cache.bulkAdd(paths.map(p => p.slice(prefixLen + 1)))
+      }))
+
+    this.register(
+      decorate.afterCall(editor.quickOpenPanel, 'removeInitFiles', ([path]) => {
+        const prefixLen = this.app.vault.path.length
+        this.cache.remove(path.slice(prefixLen + 1))
+      }))
+
+    editor.quickOpenPanel.cacheFolder(this.app.vault.path)
+
     this.cache.clear()
-    this.cacheAllFilePath()
 
     this.register(
       this.app.vault.on('mounted', () => {
         this.cache.clear()
-        this.cacheAllFilePath()
       }))
 
+
+    // feat: support open wikilink
     this.register(
       decorate(editor, 'tryOpenLink', fn => ($a, param1) => {
         if (!$a.attr('href')) {
@@ -64,19 +83,6 @@ export default class WikilinkPlugin extends Plugin<WikilinkSettings> {
 
         return fn($a, param1)
       }))
-  }
-
-  private cacheAllFilePath() {
-    return new Promise((resolve, reject) => {
-      const pattern = `**/*{.textbundle/text,}.{md,markdown}`
-      const opts = { cwd: this.app.vault.path, nodir: true }
-      // @ts-ignore
-      glob(pattern, opts, (err, files) => {
-        if (err) return reject(err)
-        this.cache.bulkAdd(files)
-        resolve(files.length)
-      })
-    })
   }
 
   open(wikiLink: string) {
@@ -92,62 +98,16 @@ export default class WikilinkPlugin extends Plugin<WikilinkSettings> {
 
     // handle: fileName
     if (file) {
-      this.resolveFilePath(file)
-        .then(filePath => {
-          filePath && editor.library.openFile(filePath)
-        })
+      const filepath = this.cache.match(file)
+      if (filepath) {
+        editor.library.openFile(path.join(this.app.vault.path, filepath))
+      }
+      else {
+        new Notice(this.i18n.t.notSuchFile + file)
+      }
     }
 
     // handle: anchor
     setTimeout(() => this.app.openLink('#' + anchor), 500)
-  }
-
-  async resolveFilePath(fileName: string) {
-    let filePath = this.cache.match(fileName)
-    if (filePath) {
-      try {
-        const absFilePath = path.join(this.app.vault.path, filePath)
-        fs.accessSync(absFilePath)
-        return absFilePath
-      } catch (error) {
-        this.cache.remove(filePath)
-      }
-    }
-
-    filePath = await this.searchFilePath(fileName)
-    if (!filePath) {
-      return
-    }
-
-    this.cache.add(filePath)
-    return path.join(this.app.vault.path, filePath)
-  }
-
-  private searchFilePath(fileName: string): Promise<string | undefined> {
-    const dirname = path.dirname(this.app.workspace.activeFile)
-    const absFilePath = path.join(dirname, `${fileName}.md`)
-    if (fs.existsSync(absFilePath)) {
-      return Promise.resolve(this.relativeFromRoot(absFilePath))
-    }
-
-    return new Promise((resolve, reject) => {
-      const pattern = `**/${fileName}{.textbundle/text,}.{md,markdown}`
-      const opts = { cwd: this.app.vault.path, nodir: true }
-      new glob.Glob(pattern, opts)
-        .on('match', (file: string) => {
-          resolve(file)
-        })
-        .on('end', (matches: string[]) => {
-          if (matches.length === 0) {
-            new Notice(this.i18n.t.notSuchFile + fileName)
-            return reject()
-          }
-          resolve(matches[0])
-        })
-    })
-  }
-
-  private relativeFromRoot(filePath: string) {
-    return path.relative(this.app.vault.path, filePath)
   }
 }
