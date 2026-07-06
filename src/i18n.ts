d77fbaf932ce0757ccb515d6c7f7a9b0e2ffd28a
plylrnsdy <plylrnsdy@163.com>
2026-07-07 00:30:23 +0800

    chore(i18n): extract i18n resources from ts to external json files

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,35 +1,7 @@
 import { I18n } from "@typora-community-plugin/core"
+import * as Locale from './locales/lang.en.json'
 
 
-export const i18n = new I18n({
-  resources: {
-    'en': {
-      commandToggle: 'Toggle Focused/Selected Text Wikilink Style',
-      notWikilink: 'Wikilink should be sourrounded by `[[` and `]]` !',
-      notSuchFile: 'Can not found the file: ',
-      useSuggest: {
-        name: 'Use suggestion',
-        desc: 'Input text prefix `[[` to trigger wikilink suggestions.'
-      },
-      headingAdditional: 'Additional',
-      useInFileExplorer: {
-        name: 'Use in file explorer',
-        desc: 'Click file `[[name]].md` will open `name.md`',
-      },
-    },
-    'zh-cn': {
-      commandToggle: '切换 Wikilink 样式',
-      notWikilink: 'Wikilink 应该使用 `[[` 和 `]]` 包围！',
-      notSuchFile: '找不到文件: ',
-      useSuggest: {
-        name: '输入建议',
-        desc: '输入触发字符 `[[` 触发 wikilink 建议。'
-      },
-      headingAdditional: '附加功能',
-      useInFileExplorer: {
-        name: '在文件管理器中使用 Wikilink',
-        desc: '点击文件 `[[name]].md` 将会打开 `name.md`',
-      },
-    },
-  }
+export const i18n = new I18n<typeof Locale>({
+  localePath: 'locales'
 })
